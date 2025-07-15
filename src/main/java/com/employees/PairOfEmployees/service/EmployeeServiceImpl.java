29c0a5e4293aa70a823a29a4563a720fe705ca3a
mmarin0v <mariyanmarin0v92@gmail.com>
2025-07-15 22:00:14 +0300

    Add better space removing, add logs, fix download csv headers, add test file

--- src/main/java/com/employees/PairOfEmployees/service/EmployeeServiceImpl.java
+++ src/main/java/com/employees/PairOfEmployees/service/EmployeeServiceImpl.java
@@ -32,6 +32,7 @@ public class EmployeeServiceImpl implements EmployeeService {
             .appendOptional(DateTimeFormatter.ofPattern("dd-MM-yyyy"))
             .toFormatter();
     private static final String[] HEADERS = { "EmpID", "ProjectID", "DateFrom", "DateTo" };
+    private static final String[] DOWNLOAD_HEADERS = { "Employee ID #1", "Employee ID #2", "Project ID", "Days worked" };
 
     public List<Team> findPartnership(MultipartFile csvFile, ReportType reportType) {
         List<EmployeeProjectRecord> csvRecords = parseCSV(csvFile);
@@ -44,24 +45,27 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         CSVFormat csvFormat = CSVFormat.DEFAULT.builder()
                 .setHeader(HEADERS)
-                .setSkipHeaderRecord(true).get();
+                .setSkipHeaderRecord(true)
+                .setTrim(true)
+                .get();
 
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
             CSVParser csvParser = CSVParser.parse(reader, csvFormat);
 
             for (CSVRecord csvRecord : csvParser) {
                 EmployeeProjectRecord record = new EmployeeProjectRecord();
-                record.setEmployeeId(Long.parseLong(csvRecord.get("EmpID").trim()));
-                record.setProjectID(Long.parseLong(csvRecord.get("ProjectID").trim()));
-                record.setDateFrom(parseDate(csvRecord.get("DateFrom").trim()));
-                String dateTo = csvRecord.get("DateTo").trim();
-                record.setDateTo(dateTo == null || dateTo.equalsIgnoreCase("NULL") ? LocalDate.now() : parseDate(dateTo));
+                record.setEmployeeId(Long.parseLong(csvRecord.get("EmpID")));
+                record.setProjectID(Long.parseLong(csvRecord.get("ProjectID")));
+                record.setDateFrom(parseDate(csvRecord.get("DateFrom")));
+                String dateTo = csvRecord.get("DateTo");
+                record.setDateTo(dateTo.equalsIgnoreCase("NULL") ? LocalDate.now() : parseDate(dateTo));
 
                 records.add(record);
             }
             logger.info("File is parsed and added to List");
             return records;
         } catch (IOException e) {
+            logger.error("Failed to parse CSV: {}", e.getMessage(), e);
             throw new EmployeeServiceException("Failed to parse file", e);
         }
     }
@@ -72,8 +76,7 @@ public class EmployeeServiceImpl implements EmployeeService {
              CSVPrinter csvPrinter = new CSVPrinter(
                      new OutputStreamWriter(byteArrayOutputStream, StandardCharsets.UTF_8),
                      CSVFormat.DEFAULT.builder()
-                             .setHeader(HEADERS)
-                             .setSkipHeaderRecord(true).get())) {
+                             .setHeader(DOWNLOAD_HEADERS).get())) {
             for (Team team : partnerships) {
                 csvPrinter.printRecord(
                         team.getEmployeeId1(),
@@ -94,6 +97,7 @@ public class EmployeeServiceImpl implements EmployeeService {
         try {
             return LocalDate.parse(dateString, DATE_FORMATTER);
         } catch (DateTimeParseException e) {
+            logger.error("Failed to parse date: {}", e.getMessage(), e);
             throw new IllegalArgumentException("Not valid date format: " + dateString);
         }
     }
@@ -108,17 +112,21 @@ public class EmployeeServiceImpl implements EmployeeService {
 
                 if (employee1.getProjectID().equals(employee2.getProjectID())) {
 
-                    LocalDate start = employee1.getDateFrom().isAfter(employee2.getDateFrom()) ?
+                    LocalDate startDate = employee1.getDateFrom().isAfter(employee2.getDateFrom()) ?
                             employee1.getDateFrom() : employee2.getDateFrom();
+                    logger.info("Start Date {}", startDate);
 
-                    LocalDate end = employee1.getDateTo().isBefore(employee2.getDateTo()) ?
+                    LocalDate endDate = employee1.getDateTo().isBefore(employee2.getDateTo()) ?
                             employee1.getDateTo() : employee2.getDateTo();
+                    logger.info("End Date {}", startDate);
 
-                    if (start.isBefore(end)) {
-                        long days = ChronoUnit.DAYS.between(start, end);
+                    if (startDate.isBefore(endDate)) {
+                        long days = ChronoUnit.DAYS.between(startDate, endDate);
                         Team team = new Team(employee1.getEmployeeId(), employee2.getEmployeeId(), employee1.getProjectID(), days);
-                        System.out.println("Created pair: " +
-                                ", ProjectID=" + team.getProjectID() + ", Days=" + days);
+
+                        logger.info("Team: Employee 1 - " + employee1.getEmployeeId() +
+                                ", Employee 2 - " + employee2.getEmployeeId() +
+                                ", ProjectID - " + team.getProjectID() + ", Days - " + days);
                         if (days > maxDayInPartnership.getDaysWorked()) {
                             maxDayInPartnership = team;
                         }
